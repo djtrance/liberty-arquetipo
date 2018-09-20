ac66f27db4b1ee93b441b9f236386fc67657c533
Juan Andres <juanandres.s@tcs.com>
2018-09-20 16:00:46 -0300

    Se arregla el filtro de columnas en data grid

--- src/containers/DataGrid.js
+++ src/containers/DataGrid.js
@@ -1,76 +1,126 @@
 import React, { Component } from 'react';
 import ReactDataGrid from 'react-data-grid';
 //import Toolbar from 'react-data-grid';
-//import Data from 'react-data-grid-addons';
-import { properties } from '../properties/properties';
 
+import { properties } from '../properties/properties';
+const { Toolbar, Data: { Selectors } } = require('react-data-grid-addons')
 class DataGrid extends Component {
     constructor(props, context) {
-        super(props, context);
-        this.createRows();
-    
-        
-        this._columns = 
-        [
-          { key: 'id', name: 'ID' },
-          { key: 'title', name: 'Title' ,filterable: true, },
-          { key: 'count', name: 'Count' } ];
-            
-        this.state = null;
-      }
-    
-      createRows = () => {
-        let rows = [];
-        for (let i = 1; i < 1000; i++) {
-          rows.push({
-            id: i,
-            title: 'Title ' + i,
-            count: i * 1000
-          });
+      super(props, context);
+      this._columns = [
+        {
+          key: 'id',
+          name: 'ID',
+          width: 80
+        },
+        {
+          key: 'task',
+          name: 'Title',
+          filterable: true,
+          sortable: true
+        },
+        {
+          key: 'priority',
+          name: 'Priority',
+          filterable: true,
+          sortable: true
+        },
+        {
+          key: 'issueType',
+          name: 'Issue Type',
+          filterable: true,
+          sortable: true
+        },
+        {
+          key: 'complete',
+          name: '% Complete',
+          filterable: true,
+          sortable: true
+        },
+        {
+          key: 'startDate',
+          name: 'Start Date',
+          filterable: true,
+          sortable: true
+        },
+        {
+          key: 'completeDate',
+          name: 'Expected Complete',
+          filterable: true,
+          sortable: true
         }
-    
-        this._rows = rows;
-      };
-    
-      rowGetter = (i) => {
-        return this._rows[i];
-      };
-    
-    
-  handleFilterChange = (filter) => {
-    let newFilters = Object.assign({}, this.state.filters);
-    if (filter.filterTerm) {
-      newFilters[filter.column.key] = filter;
-    } else {
-      delete newFilters[filter.column.key];
+      ];
+  
+      this.state = { rows: this.createRows(1000), filters: {}, sortColumn: null, sortDirection: null };
     }
-    this.setState({ filters: newFilters });
-  };
-  enableFilter(){
-    this.enableFilter=true;
-  }
-  onClearFilters = () => {
-    // all filters removed
-    this.setState({filters: {} });
-  };
+  
+    getRandomDate = (start, end) => {
+      return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString();
+    };
+  
+    createRows = (numberOfRows) => {
+      let rows = [];
+      for (let i = 1; i < numberOfRows; i++) {
+        rows.push({
+          id: i,
+          task: 'Task ' + i,
+          complete: Math.min(100, Math.round(Math.random() * 110)),
+          priority: ['Critical', 'High', 'Medium', 'Low'][Math.floor((Math.random() * 3) + 1)],
+          issueType: ['Bug', 'Improvement', 'Epic', 'Story'][Math.floor((Math.random() * 3) + 1)],
+          startDate: this.getRandomDate(new Date(2015, 3, 1), new Date()),
+          completeDate: this.getRandomDate(new Date(), new Date(2016, 0, 1))
+        });
+      }
+      return rows;
+    };
+  
+    getRows = () => {
+      return Selectors.getRows(this.state);
+    };
+  
+    getSize = () => {
+      return this.getRows().length;
+    };
+  
+    rowGetter = (rowIdx) => {
+      const rows = this.getRows();
+      return rows[rowIdx];
+    };
+  
+    handleGridSort = (sortColumn, sortDirection) => {
+      this.setState({ sortColumn: sortColumn, sortDirection: sortDirection });
+    };
+  
+    handleFilterChange = (filter) => {
+      let newFilters = Object.assign({}, this.state.filters);
+      if (filter.filterTerm) {
+        newFilters[filter.column.key] = filter;
+      } else {
+        delete newFilters[filter.column.key];
+      }
+  
+      this.setState({ filters: newFilters });
+    };
+  
+    onClearFilters = () => {
+      this.setState({ filters: {} });
+    };
     render() {
   
         
         return (
             
-            <ReactDataGrid 
-            inicio={this.getInitialState} 
-            columns={this._columns}
-            rowGetter={this.rowGetter}
-            enableCellSelect={true}
-            toolbar={<button type="button" onClick="this.enableFilter={true}">Enable Filter </button>}
-            onAddFilter={this.handleFilterChange}
-            onClearFilters={this.onClearFilters}
-            rowsCount={this._rows.length}
-            minHeight={500} >
-            <h1>{{properties}}</h1>
-            </ReactDataGrid>
-        );
+<ReactDataGrid
+        onGridSort={this.handleGridSort}
+        enableCellSelect={true}
+        columns={this._columns}
+        rowGetter={this.rowGetter}
+        rowsCount={this.getSize()}
+        minHeight={500}
+        toolbar={<Toolbar enableFilter={true}/>}
+        onAddFilter={this.handleFilterChange}
+        onClearFilters={this.onClearFilters} />);
+        
     }
 }
 export default DataGrid;
