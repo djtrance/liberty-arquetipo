058b9b0b38307740f2e7ef3d7eda27403d29e53a
Gricelis Parra <gricelisparra@gmail.com>
2018-09-26 15:38:29 -0300

    Ajuste

--- src/containers/DefaultLayout/DefaultSidebar.js
+++ src/containers/DefaultLayout/DefaultSidebar.js
@@ -17,6 +17,7 @@ class DefaultSidebar extends Component {
     setOption(option){
         this.setState({
             option:         option,
+            suboption:      null,
             showOpcMant:    this.opcionMantenedor(option) ? !this.state.showOpcMant : false
         });
     }
